fb7f5001ec1f52320743dfbd242150c58e92de37
cjh <492795090@qq.com>
2020-04-21 11:06:06 +0800

    update

--- mypycorrector/rule_bert_word/config.py
+++ mypycorrector/rule_bert_word/config.py
@@ -4,7 +4,7 @@
 @Author: cjh <492795090@qq.com>
 @Date: 2020-01-03 19:03:59
 @LastEditors: cjh <492795090@qq.com>
-@LastEditTime: 2020-04-14 23:07:58
+@LastEditTime: 2020-04-20 23:06:46
 '''
 import os
 
@@ -27,7 +27,7 @@ same_pinyin_path = os.path.join(pwd_path, '../data/2013_simple_pinyin.txt')
 # 形似字
 same_stroke_path = os.path.join(pwd_path, '../data/2013_simple_shape.txt')
 # language model path
-language_model_path = os.path.join(pwd_path, '../data/kenlm/zh_giga.no_cna_cmn.prune01244.klm')#people2014corpus_chars.klm  #zh_giga.no_cna_cmn.prune01244  #THUCNews_people2014_merge_char.klm
+language_model_path = os.path.join(pwd_path, '../data/kenlm/people2014corpus_chars.klm')#people2014corpus_chars.klm  #zh_giga.no_cna_cmn.prune01244  #THUCNews_people2014_merge_char.klm
 # 用户自定义错别字混淆集  format:变体	本体   本体词词频（可省略）
 custom_confusion_path = os.path.join(pwd_path, '../data/custom_confusion.txt')
 # 用户自定义分词词典  format: 词语 词频
